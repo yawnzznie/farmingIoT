864b2040060e902976014d22cef20e96c4450058
yawnzznie <yawnzzn1313@gmail.com>
2025-05-30 15:57:54 +0700

    Update smart_farming.ino

--- smart_farming.ino
+++ smart_farming.ino
@@ -2,22 +2,20 @@
 #define BLYNK_TEMPLATE_NAME "smartfarm"
 #define BLYNK_AUTH_TOKEN "4RhGWUxAftbz3WeFFJ-R6niQcwHYL7NM"
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 char ssid[] = "naganaga";
 char pass[] = "buahlah";
 
-#define LDR_PIN 36         // Sensor cahaya (ADC1)
-#define LED_PIN 13         // Simulasi lampu pertanian
-#define BUZZER_PIN 12      // Buzzer alarm
+#define LDR_PIN A0         
+#define LED_PIN D7       // GPIO13
+#define BUZZER_PIN D6    // GPIO12
 
 int ldrValue = 0;
-bool ledManualState = true; 
+bool ledManualState = true;
 
 BlynkTimer timer;
-
 BLYNK_WRITE(V0) {
   ledManualState = param.asInt();
   digitalWrite(LED_PIN, ledManualState);
@@ -26,15 +24,16 @@ BLYNK_WRITE(V0) {
 void checkLampAndLDR() {
   ldrValue = analogRead(LDR_PIN);
   Blynk.virtualWrite(V1, ldrValue); 
+
   bool ledStatus = digitalRead(LED_PIN);
 
-  if (ldrValue < 250) { 
+  if (ldrValue < 250) {
     if (ledStatus == LOW) {
-      tone(BUZZER_PIN, 1000); 
+      tone(BUZZER_PIN, 1000);
       Blynk.virtualWrite(V2, 255); 
     } else {
       noTone(BUZZER_PIN);
-      Blynk.virtualWrite(V2,0);
+      Blynk.virtualWrite(V2, 0);
     }
   } else {
     noTone(BUZZER_PIN);
@@ -47,13 +46,12 @@ void setup() {
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(LDR_PIN, INPUT);
 
   digitalWrite(LED_PIN, ledManualState);
-  digitalWrite(BUZZER_PIN, LOW);
+  noTone(BUZZER_PIN);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  timer.setInterval(1000L, checkLampAndLDR); 
+  timer.setInterval(1000L, checkLampAndLDR);
 }
 
 void loop() {
